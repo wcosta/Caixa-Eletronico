aa1e4dbb21fb1c3f85145d55d742244236acf15b
71306587 <71306587@090528-P33S07.macklabs.br>
2013-04-15 20:58:49 -0300

    Primeira classe

---
+++ CaixaEletronico/src/negocio/to/Transacao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio.to;
+
+/**
+ *
+ * @author 71306587
+ */
+public class Transacao {
+    public void cancelarOperacao(){};
+}
